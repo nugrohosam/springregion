448255cff96417c6499e4daccc1c59cc2b7df8be
nugroho <nugroho@mackbookpro.local>
2021-04-18 06:04:29 +0700

    Initial | Commit

---
+++ src/main/java/com/nugrohosamiyono/springregion/SpringregionApplication.java
@@ -0,0 +1,13 @@
+package com.nugrohosamiyono.springregion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringregionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringregionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nugrohosamiyono/springregion/SpringregionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nugrohosamiyono.springregion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringregionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
